45207366f988441e2ffe63b6cb005a6e70d47e59
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-10-03 20:01:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
-var canvas=document.getElementById("meuCanvas");
-var contexto=canvas.getContext("2d");
-var alturaDoCarro="10px"
-var larguraDoCarro="5px"
+var canvas=document.getElementById("myCanvas");
+var context=canvas.getContext("2d");
 
-backgroundImage = "parkingLot.jpg";
-greencarImage = "car2.png";
+var largura=90;
+var altura=170;
 
+var backgroundImage = "parkingLot.jpg";
+var greencarImage = "car2.png";
+
+var x=5;
+var y=225;
+
+var planoDeFundo;
+var carrinho;
 
 function add() {
-	//carregar carro e imagens de fundo na tela.
+	planoDeFundo=new Image();
+	planoDeFundo.onload=uploadBackground;
+	planoDeFundo.src=backgroundImage;
+
+	carrinho=new Image();
+	carrinho.onload=uploadGreenCar;
+	carrinho.src=greencarImage;
 }
 
 function uploadBackground() {
-	//Defina a função ‘uploadBackground’
-
+	context.drawImage(planoDeFundo, 0, 0, canvas.width ,canvas.height);
 }
 
 function uploadGreenCar() {
-	onload=""
-
-	
+	context.drawImage(carrinho, x, y, largura ,altura);
 }
 
 
@@ -27,59 +36,68 @@ window.addEventListener("keydown", myKeyDown);
 
 function myKeyDown(e)
 {
-	keyPressed = e.keyCode;
+var keyPressed = e.keyCode;
 	console.log(keyPressed);
 		if(keyPressed == '38')
 		{
-			cima();
-			console.log("cima");
+			up();
+			console.log("up");
 		}
 	
 		if(keyPressed == '40')
 		{
-			baixo();
-			console.log("baixo");
+			down();
+			console.log("down");
 		}
 		
 		if(keyPressed == '37')
 		{
-			esquerda();
-			console.log("esquerda");
+			left();
+			console.log("left");
 		}
 	
 		if(keyPressed == '39')
 		{
-			direita();
-			console.log("direita");
+			right();
+			console.log("right");
 		}
 		
 		
 }
 
-function cima()
+function up()
 {
-	if (tecla=="38") {
-		console.log("cima");
-		cima();
+	if(y>=0) {
+		y = y - 10
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
 
-function baixo()
+function down()
 {
-	if (tecla=="40") {
-		console.log("baixo");
-		baixo();
+	
+	if(y<=500) {
+		y = y + 10
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
 
-function esquerda()
+function left()
 {
-	if (tecla=="37") {
-		console.log("esquerda");
-		esquerda();
+	if(x>=0) {
+		x = x - 10
+		uploadBackground();
+		uploadGreenCar();
+	}
 }
 
-function direita() {
-	if (tecla=="39") {
-		console.log("direita");
-		direita();
+function right()
+{
+	if(x<=900) {
+		x = x + 10
+		uploadBackground();
+		uploadGreenCar();
 	}
 }
